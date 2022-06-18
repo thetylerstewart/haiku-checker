eed944445bbaef494cadadff05cc687888597ad0
Tyler Stewart <mr.tstew@gmail.com>
2022-06-18 12:31:18 -0700

    add form

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new HtmlWebpackPlugin({
-      title: 'Insert Title',
+      title: 'Haiku Checker',
       template: './src/index.html',
       inject: 'body'
     })
